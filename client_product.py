67e39cd499551e403fc94ee1d2b61e6b784afd16
ROS <ros@cca.cz>
2024-06-28 15:46:20 +0200

    Connecting to server but doing nothing.

--- client_product.py
+++ client_product.py
@@ -4,7 +4,7 @@ import numpy as np
 import sys
 import math
 import torch
-from client import Client, DEFAULT_PORT
+from client import Client, DEFAULT_PORT, JOINTS
 from ddpg import DDPG  # Make sure DDPG class is properly imported
 
 TABLE_HEIGHT = 1.0
@@ -12,9 +12,7 @@ TABLE_THICKNESS = 0.08
 TABLE_LENGTH = 2.4
 TABLE_WIDTH = 1.4
 
-class Buffer:
-    def __init__(self, capacity):
-        mem=[]
+
 class ActionSpace:
     def __init__(self, low, high):
         self.low = np.array(low)
@@ -22,10 +20,42 @@ class ActionSpace:
         self.shape = self.low.shape
 
 ACTION_SPACE = ActionSpace(
-    [-0.3, -0.8, -10, -math.pi / 2, -10, -math.pi * 3 / 4, -10, -math.pi * 3 / 4, -10, -math.pi * 3 / 4, -10],
-    [0.3, 0.8, 10, math.pi / 2, 10, math.pi * 3 / 4, 10, math.pi * 3 / 4, 10, math.pi * 3 / 4, 10])
+    [
+        -0.3,
+        -0.8,
+        -10,
+        -math.pi / 2,
+        -10,
+        -math.pi * 3 / 4,
+        -10,
+        -math.pi * 3 / 4,
+        -10,
+        -math.pi * 3 / 4,
+        -10],
+    [
+        0.3,
+        0.8,
+        10,
+        math.pi / 2,
+        10,
+        math.pi * 3 / 4,
+        10,
+        math.pi * 3 / 4,
+        10,
+        math.pi * 3 / 4,
+        10])
 
 
+def get_neutral_joint_position():
+    jp = [0.0]*JOINTS
+    jp[0] = -0.3
+    jp[2] = math.pi
+    a = math.pi/3.8
+    jp[5] = a
+    jp[7] = a
+    jp[9] = math.pi/3.5
+    jp[10] = math.pi/2
+    return jp
 
 def run(cli):
     gamma = 0.7
@@ -50,7 +80,7 @@ def run(cli):
 
         for step in range(max_steps):
             # Calculate action
-            action = actor.calc_action(state)
+            action = actor.calc_action(state, np.random.normal(0,1,100))
 
             # Perform action in environment
             cli.send_joints(action.cpu().numpy()[0])
@@ -63,7 +93,7 @@ def run(cli):
             done = is_done()  # Example function to check if episode is done
 
             # Store transition in replay buffer
-            transition = (state, action,reward, next_state,done)
+            transition = (state, action, reward, next_state, done)
             replay_buffer.append(transition)
 
             # Update state
@@ -93,7 +123,12 @@ def run(cli):
 
 
 def get_reward(states):
-    return -((states[11] - states[17]) ** 2 + (states[13] - states[19]) ** 2)
+    ball_x = states[17]
+    ball_y = states[19]
+    paddle_x = states[11]
+    paddle_y = states[13]
+
+    return -((paddle_x - ball_x) ** 2 + (paddle_y - ball_y) ** 2)
 
 
 def is_done():
